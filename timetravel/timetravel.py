2eec44960d13c4c7a3a3f6965503054c24a62a44
Micheal Beatty <mike@mwbeatty.org>
2017-01-28 11:39:52 -0600

    add functions to separate out calculations

--- timetravel/timetravel.py
+++ timetravel/timetravel.py
@@ -11,20 +11,32 @@ BETELGEUSE = 309
 ANDROMEDA_GALAXY = 2000000
 
 
+def calculate_factor(vel):
+    if vel < 100:
+        velocity = SPEED_OF_LIGHT * (vel/100)
+    else:
+        velocity = 1
+    factor = 1 / math.sqrt(1 - (velocity ** 2 / SPEED_OF_LIGHT ** 2))
+    return factor
+
+def calculate_weight(fact):
+    return SHIPS_WEIGHT * fact
+
+
+def calculate_travel(fact, light_years):
+    return light_years / fact
+
 def main():
+
     velocity_str = input("Please enter velocity (percentage of speed of light): ")
     velocity_float = float(velocity_str)
+    factor = calculate_factor(velocity_float)
 
-    velocity = SPEED_OF_LIGHT * (velocity_float/100)
-
-    factor = 1 / math.sqrt(1 - (velocity ** 2 / SPEED_OF_LIGHT ** 2))
-
-    print(factor)
-    print("Weight is ", SHIPS_WEIGHT * factor)
-    print("Perceived travel time to Alpha Centauri: ", ALPHA_CENTAURI / factor)
-    print("Perceived travel time to Barnard's Star: ", BARNARDS_STAR / factor)
-    print("Perceived travel time to Betelgeuse: ", BETELGEUSE/ factor)
-    print("Perceived time to travel to Andromeda Galaxy", ANDROMEDA_GALAXY / factor)
+    print("Weight is ", calculate_weight(factor))
+    print("Perceived travel time to Alpha Centauri: ", calculate_travel(factor, ALPHA_CENTAURI))
+    print("Perceived travel time to Barnard's Star: ", calculate_travel(factor, BARNARDS_STAR))
+    print("Perceived travel time to Betelgeuse: ", calculate_travel(factor, BETELGEUSE))
+    print("Perceived time to travel to Andromeda Galaxy", calculate_travel(factor, ANDROMEDA_GALAXY))
 
 
 if __name__ == '__main__':
