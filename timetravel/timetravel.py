482bd280ee81dedf832426f49ab48119ce98fa4e
Micheal Beatty <mike@mwbeatty.org>
2017-01-28 11:14:43 -0600

    Add travel time to other locations

--- timetravel/timetravel.py
+++ timetravel/timetravel.py
@@ -5,10 +5,27 @@ import math
 SPEED_OF_LIGHT = 299792458 # meters/second
 SHIPS_WEIGHT = 70000
 
-velocity = SPEED_OF_LIGHT * (50/100) # percentage of speed of light
+ALPHA_CENTAURI = 4.3
+BARNARDS_STAR = 6.0
+BETELGEUSE = 309
+ANDROMEDA_GALAXY = 2000000
 
-factor = 1 / math.sqrt(1 - (velocity ** 2 / SPEED_OF_LIGHT ** 2))
 
-print(factor)
-print("Weight is ", SHIPS_WEIGHT * factor)
-print("Time to Alpha Centauri: ", 4.3 / factor)
+def main():
+    velocity_str = input("Please enter velocity (percentage of speed of light): ")
+    velocity_float = float(velocity_str)
+
+    velocity = SPEED_OF_LIGHT * (velocity_float/100)
+
+    factor = 1 / math.sqrt(1 - (velocity ** 2 / SPEED_OF_LIGHT ** 2))
+
+    print(factor)
+    print("Weight is ", SHIPS_WEIGHT * factor)
+    print("Perceived travel time to Alpha Centauri: ", ALPHA_CENTAURI / factor)
+    print("Perceived travel time to Barnard's Star: ", BARNARDS_STAR / factor)
+    print("Perceived travel time to Betelgeuse: ", BETELGEUSE/ factor)
+    print("Perceived time to travel to Andromeda Galaxy", ANDROMEDA_GALAXY / factor)
+
+
+if __name__ == '__main__':
+    main()
